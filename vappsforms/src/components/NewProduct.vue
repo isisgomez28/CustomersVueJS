ddc527657f248a59ca5e499f362de03ff9adb50a
unknown <igomezp.28@gmail.com>
2019-10-05 06:01:30 -0400

    some user interface adjustments

--- vappsforms/src/components/NewProduct.vue
+++ vappsforms/src/components/NewProduct.vue
@@ -1,43 +1,42 @@
 <template>
   <div class="newproduct container">
     <h1 class="page-header">New Product</h1>
-    <form v-on:submit="addNewProduct">
-        <div class="well">
-            <h4>Product Info</h4>
-            <div class="form-group">
-                <label>Code</label>
-                <input type="text" class="form-control" minlength="4" maxlength="10" required placeholder="Product Code" v-model="product.code">
-            </div>
-            <div class="form-group">
-                <label>Name</label>
-                <input type="text" class="form-control" minlength="4" maxlength="50" required placeholder="Product Name" v-model="product.name">
-            </div>
-            <div class="form-group">
-                <label>Description</label>
-                <textarea type="text" class="form-control" minlength="10" maxlength="150" required placeholder="Description" v-model="product.description"></textarea>
-            </div>            
+    
+    <div class="row">
+        <div class="col-md-8 order-md-1">
+            <form v-on:submit="addNewProduct">
+                <div class="well">
+                    <h4>Product Info</h4>
+                    <div class="form-group">
+                        <label>Code</label>
+                        <input type="text" class="form-control" minlength="4" maxlength="10" required placeholder="Product Code" v-model="product.code">
+                    </div>
+                    <div class="form-group">
+                        <label>Name</label>
+                        <input type="text" class="form-control" minlength="4" maxlength="50" required placeholder="Product Name" v-model="product.name">
+                    </div>
+                    <div class="form-group">
+                        <label>Description</label>
+                        <textarea type="text" class="form-control" minlength="10" maxlength="150" required placeholder="Description" v-model="product.description"></textarea>
+                    </div>            
+                </div>
+                <div class="well">
+                    <h4>Product Complements</h4>
+                    <div class="row">
+                        <div class="form-group col-md-6 mb-3">
+                            <label>Unit of Measurement</label>
+                            <v-select label="abbreviation" :options="unitsOfMeasurements" :reduce="abbreviation => abbreviation.id" v-model="product.unitOfMeasurementId"></v-select>
+                        </div>
+                        <div class="form-group col-md-6 mb-3">
+                            <label>Category</label>
+                            <v-select label="name" :options="categories" :reduce="name => name.id" v-model="product.categoryId"></v-select>
+                        </div>
+                    </div>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
         </div>
-        <div class="well">
-            <h4>Product Complements</h4>
-            <div class="form-group">
-                <label>Unit of Measurement</label>
-                <v-select label="abbreviation" :options="unitsOfMeasurements" :reduce="abbreviation => abbreviation.id" v-model="product.unitOfMeasurementId"></v-select>
-                <!-- <input type="text" class="form-control" placeholder="Default Unit of Measurement" v-model="product.unitOfMeasurement"> -->
-                <!-- <select class="form-control" v-model="selectedUnit" required>                    
-                    <option v-for="(unit, id) in unitsOfMeasurements" :key="unit.id">{{ unit.abbreviation }}</option>
-                </select> -->
-            </div>
-            <div class="form-group">
-                <label>Category</label>
-                <v-select label="name" :options="categories" :reduce="name => name.id" v-model="product.categoryId"></v-select>
-                <!-- <input type="text" class="form-control" placeholder="Category" v-model="product.category"> -->
-                <!-- <select class="form-control" v-model="selectedCategory" required>
-                    <option v-for="(category, id) in categories" :key="category.id">{{ category.name }}</option>
-                </select> -->
-            </div>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+    </div>
   </div>
 </template>
 
@@ -46,9 +45,11 @@ export default {
   name: 'newproduct',
   data () {
     return {
-      product: {},
-      selectedUnit: {},
-      selectedCategory: {},
+      product: {
+          code: '',
+          name: '',
+          description: ''
+      },
       unitsOfMeasurements: [],
       categories: []
     }
